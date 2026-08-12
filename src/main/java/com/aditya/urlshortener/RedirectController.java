93630640c989151c06e31ef125ad1f3e7e2d990b
Aditya1407g <gjaditya2003@gmail.com>
2026-08-12 17:28:36 +0530

    Layer 1

---
+++ src/main/java/com/aditya/urlshortener/RedirectController.java
@@ -0,0 +1,36 @@
+package com.aditya.urlshortener;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import java.net.URI;
+
+@RestController
+public class RedirectController {
+
+
+    private final UrlRepository urlRepository;
+
+
+    public RedirectController( UrlRepository urlRepository) {
+        this.urlRepository = urlRepository;
+    }
+
+    @GetMapping("/{shortCode}")
+    public ResponseEntity<Void> redirect(@PathVariable String shortCode){
+        Url url = urlRepository.findByShortCode(shortCode)
+                .orElseThrow(() -> new UrlNotFoundException(shortCode));
+
+        url.setClickCount(url.getClickCount() + 1);
+        urlRepository.save(url);
+
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .location(URI.create(url.getLongUrl()))
+                .build();
+
+    }
+}
