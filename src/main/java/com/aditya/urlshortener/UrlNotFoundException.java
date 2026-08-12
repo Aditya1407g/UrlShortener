261a155b6218e9ad803388d51d849c669ccc50ec
Aditya1407g <gjaditya2003@gmail.com>
2026-08-12 17:28:57 +0530

    Core functionality done.

---
+++ src/main/java/com/aditya/urlshortener/UrlNotFoundException.java
@@ -0,0 +1,8 @@
+package com.aditya.urlshortener;
+
+public class UrlNotFoundException extends RuntimeException{
+
+    public UrlNotFoundException(String shortCode) {
+        super("Short code not found: " + shortCode);
+    }
+}
