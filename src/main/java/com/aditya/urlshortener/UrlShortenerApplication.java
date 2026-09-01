71049ae0bf2b2ea0691f18adbe4fe11e945f3dd5
Aditya1407g <gjaditya2003@gmail.com>
2026-09-01 17:46:53 +0530

    AutoConfig changes for newer spring version

--- src/main/java/com/aditya/urlshortener/UrlShortenerApplication.java
+++ src/main/java/com/aditya/urlshortener/UrlShortenerApplication.java
@@ -2,8 +2,9 @@ package com.aditya.urlshortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
 public class UrlShortenerApplication {
 
 	public static void main(String[] args) {
